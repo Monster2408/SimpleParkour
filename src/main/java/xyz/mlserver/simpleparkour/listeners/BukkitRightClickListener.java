ab1f48457b3581a7d46bf56efc4543f79072f24b
Monster2408 <meoto2408@gmail.com>
2024-04-22 00:39:37 +0900

    up

---
+++ src/main/java/xyz/mlserver/simpleparkour/listeners/BukkitRightClickListener.java
@@ -0,0 +1,14 @@
+package xyz.mlserver.simpleparkour.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class BukkitRightClickListener implements Listener {
+
+    @EventHandler
+    public void on(PlayerInteractEvent e) {
+
+    }
+
+}
